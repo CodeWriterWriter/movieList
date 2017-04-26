16212821d9bac9fc964eb4e752020fbd33612633
Jane <madtea@hotmail.com>
2017-04-26 15:49:43 +0100

    google auth and login process finished

--- index.js
+++ index.js
@@ -1,6 +1,91 @@
 var omdb = require('omdb');
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var scopes = ['https://www.googleapis.com/auth/spreadsheets'];
 
-var movieToSearch = process.argv[2];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+   if (err) {
+     console.log(err)
+   }
+   authorize(JSON.parse(content), testSheet);
+})
+
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  })
+}
+
+
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: "online",
+    scope: scopes
+  });
+  console.log("Authorize at this url: \n" + authUrl);
+  var rl = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from the page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log("Error getting token: " + err );
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != "EEXIST") {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+function testSheet(auth) {
+  var sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get({
+    auth: auth,
+    spreadsheetId: "1bcIq0H31aC65vAuOWndj52WDNwa5aR5BP1K_XQGPG8o",
+    range: Sheet1,
+  }, function(err, response) {
+    if (err) {
+      console.log("The api returned error: " + err);
+    } else {
+      console.log(response)
+    }
+  })
+}
+
+/*var movieToSearch = process.argv[2];
 var movieToSearchYear = process.argv[3];
 
 if (movieToSearch != undefined && movieToSearch != null ) {
@@ -67,4 +152,4 @@ if (movieToSearch != undefined && movieToSearch != null ) {
 }
 else {
   console.log("Enter a search term")
-}
+}*/
